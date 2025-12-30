5b25c0339ebc2b44a467d0f8c934028da4c56242
Mr. Algorithm <11aravipratapsingh@gmail.com>
2025-12-30 16:06:07 +0530

    chnages

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,9 +1,7 @@
 export { default as VorqNav } from "./VorqNav";
 export { VorqHero } from "./VorqHero";
-export { BrandLogosRow } from "./BrandLogosRow";
 export { VideoShowcase } from "./VideoShowcase";
 export { ProjectShowcase } from "./ProjectShowcase";
-export { ProcessSection } from "./ProcessSection";
 export { VorqFooter } from "./VorqFooter";
 export { VorqFAQ } from "./VorqFAQ";
 export { HorizontalScrollSection } from "./HorizontalScrollSection";
